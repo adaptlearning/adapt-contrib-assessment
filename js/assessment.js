7359413b0e146714a7cafc1c5a6a422e3bad2c78
Brian Quinn <brian@learningpool.com>
2019-04-04 09:51:38 +0100

    Resolves #2398, adds a new 'assessment:restored' event
    This event passes the collated assessment state and is a pre-requisite for
    restoring the assessment state in the framework tracking.js module.

--- js/assessment.js
+++ js/assessment.js
@@ -136,6 +136,8 @@ define([
                 _byPageId: {},
                 _byAssessmentId: {}
             });
+
+            this._restoredCount = 0;
         },
 
         _checkAssessmentsComplete: function() {
@@ -291,12 +293,19 @@ define([
             this._assessments.push(assessmentModel);
 
             this._restoreModelState(assessmentModel);
+            this._restoredCount++;
 
             Adapt.trigger('assessments:register', state, assessmentModel);
 
             this._setPageProgress();
 
             this._setupQuestionNumbering();
+
+            if (this._restoredCount === this._assessments.length) {
+                // Since all assessments have been stored, broadcast an
+                // event which has the collated state.
+                Adapt.trigger('assessment:restored', this.getState());
+            }
         },
 
         get: function(id) {
