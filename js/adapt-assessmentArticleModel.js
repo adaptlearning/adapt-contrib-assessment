9cd08615a6493c130ba0f28f90e605f8a16dd7ec
Oliver Foster <oliver.foster@kineo.com>
2022-09-26 11:52:35 +0100

    Update js/adapt-assessmentArticleModel.js

--- js/adapt-assessmentArticleModel.js
+++ js/adapt-assessmentArticleModel.js
@@ -123,7 +123,7 @@ const AssessmentModel = {
     
     // sanity check the data
     const questions = this._getCurrentQuestionComponents();
-    if (this.get('_attemptInProgress') && questions.every(q => q.get('_isSubmitted'))) {
+    if (this.get('_attemptInProgress') && questions.every(q => q.get('_isSubmitted')) && this.get('_requireCompletionOf') !== Number.POSITIVE_INFINITY) {
       this.set('_attemptInProgress', false);
       logging.warn(`Assessment ${this.get('_id') _attemptInProgress corrected as all questions are submitted`);
     }
